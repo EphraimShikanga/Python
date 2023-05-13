6ec9709294d17b96eecc399b2cabd5f1732a7be1
EphraimShikanga <ephraimshikanga@gmail.com>
2023-05-13 15:58:30 +0300

    git&chmod

---
+++ Scripts/push.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+
+# try to get the commit message from the first command line argument
+try:
+    commit_message = sys.argv[1]
+# if there is no command line argument, ask for the commit message
+except IndexError:
+    commit_message = input("Enter your commit message: ")
+
+# execute the git add command
+subprocess.run(["git", "add", "."])
+
+# execute the git commit command with the commit message
+subprocess.run(["git", "commit", "-m", commit_message])
+
+# execute the git push command
+subprocess.run(["git", "push"])
